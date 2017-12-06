6593d08974e6b52896aa06a5ad3445813c8a34ab
Gevorg Khondkaryan <gevorg.khondkaryan@gmail.com>
2017-12-06 07:58:17 -0800

    Changed algorithm

--- analyze.py
+++ analyze.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 from datetime import datetime
 import base64
 import json
+import csv
 import pymongo
 from pymongo import MongoClient
 import os
@@ -25,26 +26,42 @@ mode_name = 'mode'
 release_year_name = 'release_year'
 track_artist_name = 'track_artist'
 genre_name = 'genre'
+track_country_name = 'track_country'
 
 def get_track_value(track_data, key):
     current_value = 0.0
+    discret_feature = False
+
     if key.startswith('mode_'):
         if track_data.get(mode_name) and 'mode_{0}'.format(track_data.get(mode_name)) == key:
             current_value = 1
+        discret_feature = True
     elif key.startswith('year_'):
         if track_data.get(release_year_name) and 'year_{0}'.format(track_data.get(release_year_name)) == key:
             current_value = 1
+        discret_feature = True
     elif key.startswith('artist_'):
         if track_data.get(track_artist_name) and 'artist_{0}'.format(track_data.get(track_artist_name)) == key:
             current_value = 1
+        discret_feature = True
     elif key.startswith('genre_'):
         if track_data.get(genre_name) and 'genre_{0}'.format(track_data.get(genre_name)) == key:
             current_value = 1
+        discret_feature = True
+    elif key.startswith('key_'):
+        if track_data.get(key_name) and 'key_{0}'.format(track_data.get(key_name)) == key:
+            current_value = 1
+        discret_feature = True
+    elif key.startswith('country_feature_'):
+        if track_data.get(track_country_name) and 'country_feature_{0}'.format(track_data.get(track_country_name)) == key:
+            current_value = 1
+        discret_feature = True
     else:
         if track_data.get(key):
             current_value = track_data.get(key)
 
-    return current_value
+    return current_value, discret_feature
+
 
 playlistsDictionary = {}
 playlistDistinctTracksDictionary = {}
@@ -62,7 +79,7 @@ for playlist in playlists:
     playlistsDictionary[id] = (uri, [], defaultdict(float), defaultdict(float), 0.0)
 
 # Load All Playlist Distinct Tracks
-playlistDistinctTracks = list(client.query("SELECT * FROM `umg-dev.pitching.track_echonest_data`").result(timeout=6000))
+playlistDistinctTracks = list(client.query("SELECT * FROM `umg-dev.pitching.playlist_tracks_echonest_data`").result(timeout=6000))
 
 # Fill Playlist Distinct Tracks to Dictionary
 for track in playlistDistinctTracks:
@@ -90,13 +107,16 @@ for track in playlistDistinctTracks:
         release_year = release_date_raw.split(' ')[0]
 
     track_artist = track[15].encode('utf-8') if track[15] else None
-    genre = track[16].encode('utf-8') if track[16] else None
+    genre = track[17].encode('utf-8') if track[17] else None
+
+    track_country = isrc[0:2]
+
     playlistDistinctTracksDictionary[isrc] ={uri_name : uri, energy_name: energy, liveness_name: liveness,
                                              tempo_name: tempo, speechiness_name: speechiness, acousticness_name: acousticness,
                                              instrumentalness_name: instrumentalness, danceability_name: danceability,
                                              key_name:key, duration_ms_name: duration_ms, loudness_name: loudness,
                                              valence_name:valence, mode_name: mode, release_year_name: release_year,
-                                             track_artist_name: track_artist, genre_name: genre}
+                                             track_artist_name: track_artist, genre_name: genre, track_country_name: track_country}
 
 # Load All Playlist Tracks
 playlistTracks = list(client.query("SELECT playlist_id, isrc FROM `umg-dev.pitching.spotify_playlists_4K_tracks_info` WHERE LENGTH(isrc) > 0").result(timeout=6000))
@@ -104,6 +124,10 @@ playlistTracks = list(client.query("SELECT playlist_id, isrc FROM `umg-dev.pitch
 # Append For Each Playlist Their Tracks
 for playlistTrack in playlistTracks:
     playlistId = playlistTrack[0].encode('utf-8')
+
+    if not playlistsDictionary.get(playlistId):
+        continue
+
     isrc = playlistTrack[1].encode('utf-8')
     playlistData = playlistsDictionary[playlistId]
     playlistData[1].append(isrc)
@@ -153,10 +177,6 @@ for playlistKey in playlistsDictionary:
         if trackData.get(valence_name):
             playlistMedian[valence_name] = playlistMedian[valence_name] + trackData[valence_name]
 
-        if trackData.get(mode_name):
-            mode_name_data = 'mode_{0}'.format(trackData[mode_name])
-            playlistMedian[mode_name_data] = playlistMedian[mode_name_data] + 1
-
         if trackData.get(release_year_name):
             release_year_name_data = 'year_{0}'.format(trackData[release_year_name])
             playlistMedian[release_year_name_data] = playlistMedian[release_year_name_data] + 1
@@ -169,6 +189,11 @@ for playlistKey in playlistsDictionary:
             genre_name_data = 'genre_{0}'.format(trackData[genre_name])
             playlistMedian[genre_name_data] = playlistMedian[genre_name_data] + 1
 
+        if trackData.get(track_country_name):
+            track_country_data = 'country_feature_{0}'.format(trackData.get(track_country_name))
+            playlistMedian[track_country_data] = playlistMedian[track_country_data] + 1
+
+
     for key, value in playlistMedian.iteritems():
         playlistMedian[key] = value / (len(playlistTracks) * 1.0)
 
@@ -180,13 +205,17 @@ for playlistKey in playlistsDictionary:
             continue
 
         for key, value in playlistMedian.iteritems():
-            track_value = get_track_value(trackData, key)
-            weight_value = (track_value - value)**2
+            track_value, discrete = get_track_value(trackData, key)
+            weight_value = 0.0
+            if discrete == True and track_value == 0:
+                weight_value = max(value, (1-value)) **2
+            else:
+                weight_value = (track_value - value)**2
             playlistWeights[key] = playlistWeights[key] + weight_value
 
     # Calculate Playlist Weights
     for key, value in playlistWeights.iteritems():
-        weight_final_value = 1/(value * 1.0) if value != 0 else float("inf")
+        weight_final_value = 1/(value * 1.0) if value != 0 else 1000000
         playlistWeights[key] = weight_final_value
 
     # Calculate Max Error Of the Playlist
@@ -201,7 +230,7 @@ for playlistKey in playlistsDictionary:
             continue
 
         for key, value in playlistMedian.iteritems():
-            track_value = get_track_value(trackData, key)
+            track_value, discrete = get_track_value(trackData, key)
             calc_sum = calc_sum + (((track_value - value) ** 2) * playlistWeights[key])
             weight_sum = weight_sum + playlistWeights[key]
 
@@ -214,7 +243,7 @@ for playlistKey in playlistsDictionary:
                                        playlistTracks, playlistMedian, playlistWeights, playlist_max_error)
 
 #load All Tracks for playlist pitching
-pitchingTracks = list(client.query("SELECT * FROM `umg-dev.pitching.pitching_tracks_echonest_data`").result(timeout=6000))
+pitchingTracks = list(client.query("SELECT * FROM `umg-dev.pitching.pitching_tracks_echonest_data_full`").result(timeout=6000))
 
 
 for pitching_track in pitchingTracks:
@@ -242,14 +271,15 @@ for pitching_track in pitchingTracks:
     if release_date_raw:
         release_year = release_date_raw.split(' ')[0]
 
-    genre = pitching_track[16].encode('utf-8') if pitching_track[16] else None
+    genre = pitching_track[17].encode('utf-8') if pitching_track[17] else None
+    track_country = isrc[0:2]
 
     pitching_track_dictionary = {uri_name : uri, energy_name: energy, liveness_name: liveness,
                                              tempo_name: tempo, speechiness_name: speechiness, acousticness_name: acousticness,
                                              instrumentalness_name: instrumentalness, danceability_name: danceability,
                                              key_name:key, duration_ms_name: duration_ms, loudness_name: loudness,
                                              valence_name:valence, mode_name: mode, release_year_name: release_year,
-                                             track_artist_name: track_artist, genre_name: genre}
+                                             track_artist_name: track_artist, genre_name: genre, track_country_name: track_country}
 
     for playlist_to_pitch_key, playlist_to_pitch_value  in playlistsDictionary.iteritems():
         playlist_uri = playlist_to_pitch_value[0]
@@ -260,7 +290,7 @@ for pitching_track in pitchingTracks:
         calc_sum = 0.0
 
         for key, value in playlist_median.iteritems():
-            track_value = get_track_value(pitching_track_dictionary, key)
+            track_value, discrete = get_track_value(pitching_track_dictionary, key)
             calc_sum = calc_sum + (((track_value - value)**2) * playlist_weights[key])
             weight_sum = weight_sum + playlist_weights[key]
 
@@ -268,13 +298,20 @@ for pitching_track in pitchingTracks:
 
         track_error_percentage = 0.0
 
-        if track_error > playlist_max_error:
-            track_error_percentage = -1 * (100 - (100 * (playlist_max_error / track_error)))
-            if track_error_percentage <= 30:
-                result_array.append((isrc, playlist_uri, track_error_percentage))
-        else:
-            track_error_percentage = (100 - (100 * (track_error / playlist_max_error)))
-            result_array.append((isrc, playlist_uri, track_error_percentage))
+        if track_error <= playlist_max_error:
+            if playlist_max_error != 0:
+                track_error_percentage = (100 - (100 * (track_error / playlist_max_error)))
+
+                if track_error_percentage >= 30:
+                    result_array.append((isrc, playlist_uri, track_error_percentage))
+
+# Save Result to the File
+f = open('result.csv', 'w+')
+csv_file = csv.writer(f)
+for pitching_info in result_array:
+    csv_file.writerow((pitching_info[0], pitching_info[1], pitching_info[2]))
+
+f.close()
 
 
 
